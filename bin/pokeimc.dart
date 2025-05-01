3df270ef3320df0516b0b269fa949d3a449f8df0
chris k <yovirinc@gmail.com>
2025-05-01 12:45:09 +0200

    first commit

---
+++ bin/pokeimc.dart
@@ -0,0 +1,5 @@
+import 'package:pokeimc/pokeimc.dart' as pokeimc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pokeimc.calculate()}!');
+}
---
+++ test/pokeimc_test.dart
@@ -0,0 +1,8 @@
+import 'package:pokeimc/pokeimc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
